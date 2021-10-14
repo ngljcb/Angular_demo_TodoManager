1390b0a4e2c2c0904db80a8efe667af2ab39741c
Jacob Angeles <75119871+angelesjacob@users.noreply.github.com>
2021-10-15 00:02:43 +0200

    edit titles

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -10,7 +10,7 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
-  title: string = 'Todo Manager';
+  title: string = 'To-Do Manager';
   showAddTask: boolean = false;
   subscription: Subscription;
 
